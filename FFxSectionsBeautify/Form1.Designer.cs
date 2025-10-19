c6c2b3201d7796b02797a2ea3e2506f94b45411c
DESKTOP-GH3M5SF\vipff <emelnikov@scid.ru>
2025-10-19 05:36:10 +0300

    #build

---
+++ FFxSectionsBeautify/Form1.Designer.cs
@@ -0,0 +1,241 @@
+﻿namespace FFxSectionsBeautify
+{
+    partial class main_form
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(main_form));
+            this.p_main = new System.Windows.Forms.Panel();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.rtb_output = new System.Windows.Forms.RichTextBox();
+            this.rtb_input = new System.Windows.Forms.RichTextBox();
+            this.p_buttons = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
+            this.nupd_offset = new System.Windows.Forms.NumericUpDown();
+            this.button1 = new System.Windows.Forms.Button();
+            this.p_main.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.p_buttons.SuspendLayout();
+            this.tableLayoutPanel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nupd_offset)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // p_main
+            // 
+            this.p_main.AutoSize = true;
+            this.p_main.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.p_main.Controls.Add(this.tableLayoutPanel2);
+            this.p_main.Controls.Add(this.tableLayoutPanel1);
+            this.p_main.Controls.Add(this.p_buttons);
+            this.p_main.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.p_main.Location = new System.Drawing.Point(0, 0);
+            this.p_main.Name = "p_main";
+            this.p_main.Size = new System.Drawing.Size(800, 561);
+            this.p_main.TabIndex = 0;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel2.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.Single;
+            this.tableLayoutPanel2.ColumnCount = 2;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 39.57286F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 60.42714F));
+            this.tableLayoutPanel2.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.label2, 1, 0);
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 3);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 1;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(797, 20);
+            this.tableLayoutPanel2.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(4, 1);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Входной LTX";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(319, 1);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Выходной LTX";
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel1.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.Single;
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 39.67459F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 60.32541F));
+            this.tableLayoutPanel1.Controls.Add(this.rtb_output, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.rtb_input, 0, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 25);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(800, 481);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // rtb_output
+            // 
+            this.rtb_output.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.rtb_output.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.rtb_output.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.rtb_output.Location = new System.Drawing.Point(321, 4);
+            this.rtb_output.Name = "rtb_output";
+            this.rtb_output.Size = new System.Drawing.Size(475, 473);
+            this.rtb_output.TabIndex = 1;
+            this.rtb_output.Text = "";
+            // 
+            // rtb_input
+            // 
+            this.rtb_input.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.rtb_input.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.rtb_input.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.rtb_input.Location = new System.Drawing.Point(4, 4);
+            this.rtb_input.Name = "rtb_input";
+            this.rtb_input.Size = new System.Drawing.Size(310, 473);
+            this.rtb_input.TabIndex = 0;
+            this.rtb_input.Text = "";
+            // 
+            // p_buttons
+            // 
+            this.p_buttons.Controls.Add(this.label3);
+            this.p_buttons.Controls.Add(this.tableLayoutPanel3);
+            this.p_buttons.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.p_buttons.Location = new System.Drawing.Point(0, 512);
+            this.p_buttons.Name = "p_buttons";
+            this.p_buttons.Size = new System.Drawing.Size(800, 49);
+            this.p_buttons.TabIndex = 0;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(7, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(199, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Мин. Отступ перед значениями полей";
+            // 
+            // tableLayoutPanel3
+            // 
+            this.tableLayoutPanel3.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel3.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.Single;
+            this.tableLayoutPanel3.ColumnCount = 3;
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 52.32745F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 47.67255F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 248F));
+            this.tableLayoutPanel3.Controls.Add(this.button1, 2, 0);
+            this.tableLayoutPanel3.Controls.Add(this.nupd_offset, 0, 0);
+            this.tableLayoutPanel3.Location = new System.Drawing.Point(2, 13);
+            this.tableLayoutPanel3.Name = "tableLayoutPanel3";
+            this.tableLayoutPanel3.RowCount = 1;
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(800, 33);
+            this.tableLayoutPanel3.TabIndex = 3;
+            // 
+            // nupd_offset
+            // 
+            this.nupd_offset.Location = new System.Drawing.Point(4, 4);
+            this.nupd_offset.Name = "nupd_offset";
+            this.nupd_offset.Size = new System.Drawing.Size(200, 20);
+            this.nupd_offset.TabIndex = 1;
+            this.nupd_offset.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.nupd_offset.Value = new decimal(new int[] {
+            45,
+            0,
+            0,
+            0});
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button1.Location = new System.Drawing.Point(553, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(243, 25);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Форматировать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_2);
+            // 
+            // main_form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 561);
+            this.Controls.Add(this.p_main);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(800, 600);
+            this.Name = "main_form";
+            this.Text = "FFx0001 LTX Sections beautify tool version: 1.0";
+            this.p_main.ResumeLayout(false);
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.p_buttons.ResumeLayout(false);
+            this.p_buttons.PerformLayout();
+            this.tableLayoutPanel3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.nupd_offset)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel p_main;
+        private System.Windows.Forms.Panel p_buttons;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.RichTextBox rtb_output;
+        private System.Windows.Forms.RichTextBox rtb_input;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown nupd_offset;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ FFxSectionsBeautify/Form1.cs
@@ -0,0 +1,345 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+
+namespace FFxSectionsBeautify
+{
+    public partial class main_form : Form
+    {
+        public main_form()
+        {
+            InitializeComponent();
+        }
+
+        private string CleanSpaces(string input)
+        {
+            // Замена Regex на простой метод
+            if (string.IsNullOrEmpty(input)) return input;
+
+            StringBuilder result = new StringBuilder();
+            bool lastWasSpace = false;
+
+            foreach (char c in input)
+            {
+                if (c == ' ' || c == '\t')
+                {
+                    if (!lastWasSpace)
+                    {
+                        result.Append(' ');
+                        lastWasSpace = true;
+                    }
+                }
+                else
+                {
+                    result.Append(c);
+                    lastWasSpace = false;
+                }
+            }
+
+            return result.ToString().Trim();
+        }
+
+        private void processing()
+        {
+            if (rtb_input == null || rtb_input.Lines == null) return;
+
+            List<string> cleanedLines = new List<string>();
+            foreach (string line in rtb_input.Lines)
+            {
+                if (line == null) continue;
+
+                string trimmed = line.Trim();
+                if (trimmed.Length > 0)
+                {
+                    string singleSpaced = CleanSpaces(trimmed);
+                    cleanedLines.Add(singleSpaced);
+                }
+                else
+                {
+                    cleanedLines.Add("");
+                }
+            }
+
+            List<string> finalLines = CleanEmptyLines(cleanedLines);
+            List<Section> sections = ParseSections(finalLines);
+
+            if (sections.Count > 0)
+            {
+                AddCommentsToFirstSection(sections, finalLines);
+                AddCommentsToOtherSections(sections);
+                SetPaddings(sections);
+                BuildOutput(sections);
+            }
+        }
+
+        private List<string> CleanEmptyLines(List<string> lines)
+        {
+            List<string> result = new List<string>();
+            bool lastWasEmpty = false;
+
+            foreach (string line in lines)
+            {
+                if (line == null) continue;
+
+                if (line.Length == 0)
+                {
+                    if (!lastWasEmpty)
+                    {
+                        result.Add("");
+                        lastWasEmpty = true;
+                    }
+                }
+                else
+                {
+                    result.Add(line);
+                    lastWasEmpty = false;
+                }
+            }
+
+            return result;
+        }
+
+        private class Section
+        {
+            public int Padding = 0;
+            public int AdditionalLinesPadding = 5;
+            public bool isHudSection = false;
+            public List<string> Comments = new List<string>();
+            public string FullSectionFirstLine = "";
+            public List<string> Lines = new List<string>();
+
+            public string Build(int offset)
+            {
+                StringBuilder sb = new StringBuilder();
+
+                // Комментарии
+                if (Comments != null)
+                {
+                    foreach (string comment in Comments)
+                    {
+                        if (comment != null)
+                        {
+                            sb.Append(new string(' ', Padding));
+                            sb.AppendLine(comment);
+                        }
+                    }
+                }
+
+                // Заголовок секции
+                if (!string.IsNullOrEmpty(FullSectionFirstLine))
+                {
+                    sb.Append(new string(' ', Padding));
+                    sb.AppendLine(FullSectionFirstLine);
+                }
+
+                // Содержимое
+                if (Lines != null)
+                {
+                    foreach (string line in Lines)
+                    {
+                        if (line != null)
+                        {
+                            string formattedLine = FormatLineWithOffset(line, Padding + AdditionalLinesPadding, offset);
+                            sb.AppendLine(formattedLine);
+                        }
+                    }
+                }
+
+                return sb.ToString();
+            }
+
+            private string FormatLineWithOffset(string line, int basePadding, int offset)
+            {
+                if (string.IsNullOrEmpty(line))
+                    return new string(' ', basePadding);
+
+                int equalsIndex = line.IndexOf('=');
+                if (equalsIndex <= 0)
+                    return new string(' ', basePadding) + line;
+
+                string key = line.Substring(0, equalsIndex).Trim();
+                string value = line.Substring(equalsIndex).Trim();
+
+                int totalKeyLength = basePadding + key.Length;
+                int spacesNeeded = Math.Max(1, offset - totalKeyLength);
+
+                return new string(' ', basePadding) + key + new string(' ', spacesNeeded) + value;
+            }
+        }
+
+        private List<Section> ParseSections(List<string> lines)
+        {
+            List<Section> sections = new List<Section>();
+            if (lines == null) return sections;
+
+            Section currentSection = null;
+            bool lastWasEmpty = false;
+
+            foreach (string line in lines)
+            {
+                if (line == null) continue;
+
+                if (line.Length == 0)
+                {
+                    // Пустая строка внутри секции
+                    if (currentSection != null && !lastWasEmpty)
+                    {
+                        currentSection.Lines.Add("");
+                        lastWasEmpty = true;
+                    }
+                }
+                else
+                {
+                    lastWasEmpty = false;
+
+                    // Проверка на секцию
+                    if (line.StartsWith("[") && line.IndexOf(']') > 0)
+                    {
+                        currentSection = new Section();
+                        currentSection.FullSectionFirstLine = line;
+                        currentSection.isHudSection = line.IndexOf("hud", StringComparison.OrdinalIgnoreCase) >= 0;
+                        sections.Add(currentSection);
+                    }
+                    else if (currentSection != null)
+                    {
+                        // Обычная строка внутри секции
+                        currentSection.Lines.Add(line);
+                    }
+                }
+            }
+
+            return sections;
+        }
+
+        private void AddCommentsToFirstSection(List<Section> sections, List<string> allLines)
+        {
+            if (sections == null || sections.Count == 0 || allLines == null) return;
+
+            List<string> firstComments = new List<string>();
+            bool foundFirstSection = false;
+
+            for (int i = 0; i < allLines.Count && !foundFirstSection; i++)
+            {
+                string line = allLines[i];
+                if (string.IsNullOrEmpty(line)) continue;
+
+                if (line.StartsWith("[") && line.IndexOf(']') > 0)
+                {
+                    foundFirstSection = true;
+                }
+                else if (line.StartsWith(";"))
+                {
+                    firstComments.Add(line);
+                }
+            }
+
+            if (firstComments.Count > 0)
+            {
+                sections[0].Comments.AddRange(firstComments);
+            }
+        }
+
+        private void AddCommentsToOtherSections(List<Section> sections)
+        {
+            if (sections == null) return;
+
+            for (int i = 1; i < sections.Count; i++)
+            {
+                if (sections[i] == null || sections[i].isHudSection) continue;
+
+                Section prevSection = sections[i - 1];
+                if (prevSection == null || prevSection.Lines == null) continue;
+
+                List<string> trailingComments = new List<string>();
+
+                // Ищем комментарии в конце предыдущей секции
+                for (int j = prevSection.Lines.Count - 1; j >= 0; j--)
+                {
+                    string line = prevSection.Lines[j];
+                    if (string.IsNullOrEmpty(line)) continue;
+
+                    if (line.StartsWith(";"))
+                    {
+                        trailingComments.Insert(0, line);
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+
+                // Перемещаем комментарии
+                if (trailingComments.Count > 0)
+                {
+                    RemoveTrailingComments(prevSection, trailingComments.Count);
+                    sections[i].Comments.AddRange(trailingComments);
+                }
+            }
+        }
+
+        private void RemoveTrailingComments(Section section, int count)
+        {
+            if (section == null || section.Lines == null) return;
+
+            int removed = 0;
+            for (int i = section.Lines.Count - 1; i >= 0 && removed < count; i--)
+            {
+                string line = section.Lines[i];
+                if (!string.IsNullOrEmpty(line) && line.StartsWith(";"))
+                {
+                    section.Lines.RemoveAt(i);
+                    removed++;
+                }
+            }
+        }
+
+        private void SetPaddings(List<Section> sections)
+        {
+            if (sections == null) return;
+
+            foreach (Section section in sections)
+            {
+                if (section != null)
+                {
+                    section.Padding = section.isHudSection ? 5 : 0;
+                }
+            }
+        }
+
+        private void BuildOutput(List<Section> sections)
+        {
+            if (sections == null || rtb_output == null) return;
+
+            StringBuilder result = new StringBuilder();
+            int offset = (int)nupd_offset.Value;
+
+            foreach (Section section in sections)
+            {
+                if (section == null) continue;
+
+                if (result.Length > 0)
+                {
+                    result.AppendLine();
+                }
+
+                result.Append(section.Build(offset));
+            }
+
+            rtb_output.Text = result.ToString();
+        }
+
+        private void button1_Click_2(object sender, EventArgs e)
+        {
+            if (rtb_output != null)
+            {
+                rtb_output.Clear();
+            }
+
+            if (rtb_input != null && !string.IsNullOrEmpty(rtb_input.Text))
+            {
+                processing();
+            }
+        }
+    }
+}
